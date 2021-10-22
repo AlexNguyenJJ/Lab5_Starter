f5e0e03b418b86712e8815a55fa9f00c43b9be77
AlexNguyenJJ <atn012@ucsd.edu>
2021-10-21 18:02:29 -0700

    Successfully added feature to change horn image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,30 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  /* Change the image displayed based on selection */
+
+  // Get the <section> HTML element for horns
+  var hornSelect = document.getElementById("horn-select");
+  // Create an EventListener for when there is a change in horn value
+  hornSelect.addEventListener('change', (event) => {
+
+    // Select horn image using CSS for "#expose img"
+    let imgHorn = document.querySelector("#expose img")
+
+    switch (event.target.value) {
+      case "air-horn":
+        imgHorn.setAttribute("src", "assets/images/air-horn.svg");
+        break;
+      case "car-horn":
+        imgHorn.setAttribute("src", "assets/images/car-horn.svg");
+        break;
+      case "party-horn":
+        imgHorn.setAttribute("src", "assets/images/party-horn.svg");
+        break;
+      default:
+        console.log("invalid option");
+    }
+  })
+
+  
 }
