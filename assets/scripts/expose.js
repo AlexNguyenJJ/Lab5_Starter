21ee2c114dcea48d46f8c2cb42fe9ee6b4fceeec
AlexNguyenJJ <atn012@ucsd.edu>
2021-10-22 14:22:13 -0700

    Sliding audio bar functions correctly

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -20,14 +20,17 @@ function init() {
     switch (event.target.value) {
       case "air-horn":
         imgHorn.setAttribute("src", "assets/images/air-horn.svg");
+        imgHorn.setAttribute("alt", "Air horn selected");
         audioHorn.setAttribute("src", "assets/audio/air-horn.mp3");
         break;
       case "car-horn":
         imgHorn.setAttribute("src", "assets/images/car-horn.svg");
+        imgHorn.setAttribute("alt", "Car horn selected");
         audioHorn.setAttribute("src", "assets/audio/car-horn.mp3");
         break;
       case "party-horn":
         imgHorn.setAttribute("src", "assets/images/party-horn.svg");
+        imgHorn.setAttribute("alt", "Party horn selected");
         audioHorn.setAttribute("src", "assets/audio/party-horn.mp3");
         break;
       default:
@@ -35,6 +38,31 @@ function init() {
     }
   })
 
+  /* Audio Slider option */
+  var audioSlider = document.querySelector("#volume-controls input[type='range']");
+  var volumeLevelIcon = document.querySelector("#volume-controls img");
+  audioSlider.addEventListener('input', (event) => {
 
+    // Select audio file using CSS for ".hidden"
+    let audioHorn = document.querySelector(".hidden");
+
+    if (event.target.value == 0) {
+      volumeLevelIcon.setAttribute("src", "assets/icons/volume-level-0.svg");
+      volumeLevelIcon.setAttribute("alt", "Volume level 0");
+    }
+    else if (event.target.value < 33) {
+      volumeLevelIcon.setAttribute("src", "assets/icons/volume-level-1.svg");
+      volumeLevelIcon.setAttribute("alt", "Volume level 1");
+    }
+    else if (event.target.value < 67) {
+      volumeLevelIcon.setAttribute("src", "assets/icons/volume-level-2.svg");
+      volumeLevelIcon.setAttribute("alt", "Volume level 2");
+    } else {
+      volumeLevelIcon.setAttribute("src", "assets/icons/volume-level-3.svg");
+      volumeLevelIcon.setAttribute("alt", "Volume level 3");
+    }
+    // Set volume for audio
+    audioHorn.volume = (event.target.value / 100);
+  })
   
 }
