4144429ff4949d73b3ad74f317f62e9a127f2216
AlexNguyenJJ <atn012@ucsd.edu>
2021-10-22 13:49:56 -0700

    Set audio paths upon selection

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,30 +3,38 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  /* Change the image displayed based on selection */
+
+  /* Change the sound file and image displayed based on selection */
 
   // Get the <section> HTML element for horns
   var hornSelect = document.getElementById("horn-select");
-  // Create an EventListener for when there is a change in horn value
+  // Create an EventListener for when there is a change in horn value and change
+  // img accordingly
   hornSelect.addEventListener('change', (event) => {
 
     // Select horn image using CSS for "#expose img"
-    let imgHorn = document.querySelector("#expose img")
+    let imgHorn = document.querySelector("#expose img");
+    // Select audio file using CSS for ".hidden"
+    let audioHorn = document.querySelector(".hidden");
 
     switch (event.target.value) {
       case "air-horn":
         imgHorn.setAttribute("src", "assets/images/air-horn.svg");
+        audioHorn.setAttribute("src", "assets/audio/air-horn.mp3");
         break;
       case "car-horn":
         imgHorn.setAttribute("src", "assets/images/car-horn.svg");
+        audioHorn.setAttribute("src", "assets/audio/car-horn.mp3");
         break;
       case "party-horn":
         imgHorn.setAttribute("src", "assets/images/party-horn.svg");
+        audioHorn.setAttribute("src", "assets/audio/party-horn.mp3");
         break;
       default:
         console.log("invalid option");
     }
   })
+
 
   
 }
