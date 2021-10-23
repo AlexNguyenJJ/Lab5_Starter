287b9457d91f643ca52ae567c5ecaa838a1e0f55
AlexNguyenJJ <atn012@ucsd.edu>
2021-10-22 21:06:25 -0700

    Loaded and populated voice-select

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,38 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+/**
+ * Binds EventListeners
+ */
+function bindListeners() {
+
+}
+
+/**
+ * On page load, load all the available voices for SpeechSynthesizer and populate
+ * dropdown
+ */
+function loadVoices() {
+  setTimeout(() => {
+    var voices = window.speechSynthesis.getVoices();
+    var selectVoices = document.querySelector("#explore select");
+    for (let i = 0; i < voices.length; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+      if (voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      selectVoices.appendChild(option);
+    }
+  }, 100)
+
+  
+}
+
 function init() {
-  // TODO
+  window.onload = loadVoices();
 }
